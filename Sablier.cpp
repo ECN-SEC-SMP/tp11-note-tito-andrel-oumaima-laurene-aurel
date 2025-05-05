c537a80def19e5b762088817b18eced8d5fc6ecb
Aurelien GENTE <a.gente1709@gmail.com>
2025-05-05 08:30:59 +0200

    Commentaires Sablier.cpp

--- Sablier.cpp
+++ Sablier.cpp
@@ -2,44 +2,54 @@
 #include <thread>
 #include <chrono>
 
-// Constructeurs
+// ---------- Constructeurs ----------
+
+// Initialisation d'un sablier de 60 secondes
 Sablier::Sablier(){
-    // Initialisation du sablier à 60 secondes
     temps = 60;
-    // Démarrage du sablier
     fini = 0;
 }
 
+// Initialisation du sablier au temps souhaité
 Sablier::Sablier(int temps){
-    // Initialisation du sablier au temps souhaité
     this->temps = temps;
-    // Démarrage du sablier
     fini = 0;
 }
 
-// Accesseurs
+// ---------- Accesseurs ----------
+
+// Renvoie le temps en secondes
 int Sablier::getTemps(){
     return temps;
 }
 
+// Renvoie l'état du sablier
+// 0 : en cours
+// 1 : terminé
 bool Sablier::getFini(){
     return fini;
 }
 
-// Mutateurs
+// ---------- Mutateurs ----------
+
+// Parametrage du temps
 void Sablier::setTemps(int temps){
     this->temps = temps;
 }
 
+// Termine le sablier
 void Sablier::setFini(){
     this->fini = 1;
 }
 
+// Change l'état du sablier
 void Sablier::setFini(bool etat){
     this->fini = etat;
 }
 
-// Méthodes
+// ---------- Méthodes ----------
+
+// Démarrage du décompte du sablier et décompte toutes les secondes en arrière plan
 void Sablier::startDecompte(){
     // Lancer un thread qui décrémente automatiquement le sablier
     std::thread([this]() {
@@ -50,8 +60,9 @@ void Sablier::startDecompte(){
     }).detach(); // détache le thread pour qu'il fonctionne en arrière-plan
 }
 
+// Fonction de décomptage du sablier
 void Sablier::decompte(){
     if(temps > 0 && fini == 0){
-        temps--;
-    } else fini = 1;
+        temps--; // Decomptage
+    } else fini = 1; // Fin du décomptage
 }
