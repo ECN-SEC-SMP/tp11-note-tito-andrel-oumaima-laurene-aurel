874d8fd2cf3633d960ae02479f5b9a37d056fdf7
AndreLouisSt <stephantandrelouis@gmail.com>
2025-05-05 09:40:17 +0000

    main

--- main.cpp
+++ main.cpp
@@ -1,29 +1,20 @@
 #include <string>
 #include <iostream>
-#include <string>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+#include <list>
+#include <thread>
+#include <chrono>
 #include "stdlib.h"
 #include "Case.h"
 #include "Sablier.h"
 #include "plateauRicochet.h"
-#include <vector>
+#include "Robot.h"
 
 using namespace std;
 
 int main() {
-    int x = 0;
-    int y = 0;
-    int x1 = 1;
-    int y1 = 0;
-    Case case1 = Case(x,y);
-    case1.setBordsHaut(true);
-    case1.setBordsBas(true);
-    case1.setBordsGauche(true);
-    case1.setBordsDroit(true);
-    case1.setCouleur("rouge");
-    //case1.setBordsDroit(true);
-    case1.AffichageCase(x,y);
-    //case1.AffichageCase(x,y);
-    //case1.setCouleur("bleu");
-    //case1.AffichageCase(x1,y1);
+
     return 0;
 }
