8adb95ebc9b793d6bb9197190006c48cb41513de
AndreLouisSt <stephantandrelouis@gmail.com>
2025-05-06 14:52:07 +0000

    Doxygen OK

--- Objectif.cpp
+++ Objectif.cpp
@@ -1,10 +1,19 @@
-/// Bibliothèques
+///
+/// @brief Bibliothèques
+/// @details Nécéssaires pour le bon focntionnement du projet.
+///
 #include <string>
 #include <iostream>
 #include "stdlib.h"
 #include "Objectif.h"
 
-// Constructeurs
+///
+/// @brief Création d'un objectif en fonction des coordonnées X, Y ainsi que d'une couleur et d'une forme (type d'objectif)
+/// @details Création d'un objectif lié à une case. On cherche à initialiser les coordonnées (case particulière)
+/// Ainsi que les coordonnées et la forme de l'objectif.
+/// @param[in]  X, Y entier
+/// @param[in]  Couleur, Forme string
+///
 Objectif::Objectif(string Couleur, string Forme, int X, int Y)
 {
     this->couleur = Couleur;
@@ -13,19 +22,31 @@ Objectif::Objectif(string Couleur, string Forme, int X, int Y)
     this->y = Y;
 }
 
-// Accesseurs
+///
+/// @brief Accesseurs
+/// @details Fonctions permettant l'accès aux différents paramètres d'un objectif. 
+/// L'utilisateur récupère alors les informaions nécéssaires au bon fonctionnement du programme.
+///
 string Objectif::getForme() { return this->forme; }
 string Objectif::getCouleur() { return this->couleur; }
 int Objectif::getX() { return this->x; }
 int Objectif::getY() { return this->y; }
 
-// Mutateurs
+///
+/// @brief Mutateurs
+/// @details Fonctions permettant la modification des différents paramètres d'un objectif. 
+/// Permet de configurer et de modifier les paramètres spécifiques d'un objectif.
+///
 void Objectif::setForme(string setForme) { this->forme = setForme; }
 void Objectif::setCouleur(string setCouleur) { this->couleur = setCouleur; }
 void Objectif::setX(int setX) { this->x = setX; }
 void Objectif::setY(int setY) { this->y = setY; }
 
-// Méthodes
+///
+/// @brief Méthode, ObjectifAtteint
+/// @details Fonctions méthode permettant de faire ressortir une information d'objectif atteint. 
+/// @param[in] Couleur, Forme string
+///
 bool Objectif::ObjectifAtteint(string Couleur, string Forme)
 {
     if (this->getCouleur() == Couleur && this->getForme() == Forme)
@@ -38,7 +59,10 @@ bool Objectif::ObjectifAtteint(string Couleur, string Forme)
     }
 }
 
-// Destructeur
+///
+/// @brief Destructeur
+/// @details Fonction destructrice permettant de supprimer un élément, dans le cas d'un objectif atteint par exemple.
+///
 Objectif::~Objectif()
 {
     delete this;
