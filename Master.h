81c73337f4b782dbdf0f359cc502e6dce0c42c34
Titouan <titouan.copin@outlook.fr>
2025-05-06 11:48:30 +0200

    ajout Master qui gere le jeux

---
+++ Master.h
@@ -0,0 +1,32 @@
+#ifndef MASTER_H
+#define MASTER_H
+
+#include <string>
+#include <iostream>
+#include <string>
+#include "stdlib.h"
+#include "Case.h"
+#include "Sablier.h"
+#include "plateauRicochet.h"
+#include <vector>
+#include <cstdlib> // Pour srand() et rand()
+#include <ctime>   // Pour time()
+#include "Robot.h"
+
+class Master {
+    protected:
+        // Attributs
+        int Max_X;
+        int Max_Y;
+        std::vector<std::vector<Case*>> Plateau; 
+        //std::vector<std::vector<Case*>> Plateau;
+    public:
+        // Constructeurs
+        Master(int X, int Y);
+        // Destructeur
+        ~Master();
+        void Tour();
+        void Afficher();
+};
+
+#endif
