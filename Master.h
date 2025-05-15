86f0d85bb96339fb3486a3db40b51a31d6b1e040
Titouan <titouan.copin@outlook.fr>
2025-05-15 19:54:23 +0200

    fin commentaire doxygen

--- Master.h
+++ Master.h
@@ -1,3 +1,10 @@
+/**
+ * @file Master.h
+ * @brief Définition de la classe principale contrôlant le jeu
+ * @author Équipe Tito-Andrel-Oumaima-Laurene-Aurel
+ * @date Mai 2025
+ */
+
 #ifndef MASTER_H
 #define MASTER_H
 
@@ -9,55 +16,123 @@
 #include "Sablier.h"
 #include "plateauRicochet.h"
 #include <vector>
-#include <cstdlib> // Pour srand() et rand()
-#include <ctime>   // Pour time()
+#include <cstdlib>
+#include <ctime>
 #include "Robot.h"
 #include "Joueur.h"
 
-
+/**
+ * @class Master
+ * @brief Classe principale gérant le jeu Ricochet Robot
+ *
+ * Cette classe orchestre l'ensemble des éléments du jeu:
+ * gestion du plateau, des robots, des joueurs, des objectifs,
+ * et du déroulement du jeu.
+ */
 class Master {
     protected:
-        // Attributs
+        /** @brief Largeur du plateau */
         int Max_X;
+        /** @brief Hauteur du plateau */
         int Max_Y;
-        //std::vector<std::vector<Case*>> Plateau;
+        /** @brief Plateau de jeu */
         plateauRicochet* Plateau;
 
-        // Objectif
+        /** @brief Objectif actuel à atteindre */
         Objectif* objectifCourant;
+        /** @brief Indice de l'objectif courant dans la liste */
         int indiceObjectifCourant = 0;
 
-        // Robots
+        /** @brief Robot rouge */
         Robot* robotRed; 
+        /** @brief Robot vert */
         Robot* robotGreen;
+        /** @brief Robot bleu */
         Robot* robotBlue;
+        /** @brief Robot jaune */
         Robot* robotYellow;
 
-        // Joueurs
+        /** @brief Liste des joueurs */
         std::vector<Joueur*> Joueurs;
+        /** @brief Nombre de joueurs */
         int nbJoueurs;
 
-        // Sablier
+        /** @brief Sablier pour limiter le temps de réflexion */
         Sablier* sablier;
 
-        //std::vector<std::vector<Case*>> Plateau;
     public:
-        // Constructeurs
+        /**
+         * @brief Constructeur initialisant le jeu
+         * @param X Largeur du plateau
+         * @param Y Hauteur du plateau
+         */
         Master(int X, int Y);
-        // Destructeur
+        
+        /**
+         * @brief Destructeur libérant les ressources
+         */
         ~Master();
+        
+        /**
+         * @brief Lance un tour de jeu
+         */
         void Tour();
+        
+        /**
+         * @brief Gère un tour de jeu complet pour tous les joueurs
+         */
         void TourdeJeu();
+        
+        /**
+         * @brief Sélectionne un robot
+         * @return Caractère identifiant le robot ('R','G','B','Y')
+         */
         char select_Robot();
+        
+        /**
+         * @brief Affiche le plateau de jeu
+         */
         void Afficher();
+        
+        /**
+         * @brief Tire un nouvel objectif aléatoire
+         */
         void tirerObjectif();
+        
+        /**
+         * @brief Vérifie si l'objectif actuel est atteint
+         * @return true si l'objectif est atteint, false sinon
+         */
         bool getObjectifAtteint();
+        
+        /**
+         * @brief Affiche l'objectif courant
+         */
         void afficherObjectif();
+        
+        /**
+         * @brief Déplace le robot sélectionné
+         * @param Rob Identifiant du robot ('R','G','B','Y')
+         * @param nbCoups Nombre de coups annoncés
+         * @return true si l'objectif est atteint, false sinon
+         */
         bool SelectionRobot(char Rob, int nbCoups);
+        
+        /**
+         * @brief Initialise les joueurs pour la partie
+         */
         void initJoueurs();
+        
+        /**
+         * @brief Sélectionne un joueur pour le tour
+         * @return Pointeur vers le joueur sélectionné
+         */
         Joueur* selectJoueur();
+        
+        /**
+         * @brief Affiche les scores de tous les joueurs
+         */
         void AfficherScores();
-        //void InitPlateau();
 };
 
 #endif
--- plateauRicochet.h
+++ plateauRicochet.h
@@ -1,3 +1,10 @@
+/**
+ * @file plateauRicochet.h
+ * @brief Définition de la classe gérant le plateau de jeu Ricochet Robot
+ * @author Équipe Tito-Andrel-Oumaima-Laurene-Aurel
+ * @date Mai 2025
+ */
+
 #ifndef PLATEAU_H
 #define PLATEAU_H
 
@@ -7,35 +14,123 @@
 #include <vector>
 #include <algorithm>
 #include <iterator>
-#include "plateauRicochet.h"
 #include "Robot.h"
 #include "Objectif.h"
 #include <map>
+
+/**
+ * @class plateauRicochet
+ * @brief Gestion du plateau de jeu et des déplacements des robots
+ *
+ * Cette classe gère l'ensemble du plateau de jeu, les obstacles, les objectifs
+ * et les mouvements des robots sur le plateau.
+ */
 class plateauRicochet
 {
     private:
+     /** @brief Largeur du plateau */
      int Max_X;
+     /** @brief Hauteur du plateau */
      int Max_Y;
-     //vector<std::vector<Case*>> Plateau;
+     /** @brief Grille 2D contenant les cases du plateau */
      std::vector<std::vector<Case*>> Plateau;
-     std::vector<Objectif*> Objectifs; // Vecteur d'objectifs
+     /** @brief Liste des objectifs à atteindre */
+     std::vector<Objectif*> Objectifs;
+     /** @brief Positions des obstacles déjà placés sur le plateau */
      std::vector<std::pair<int, int>> obstaclesPlaces;
     public:
+        /**
+         * @brief Constructeur initialisant le plateau
+         * @param x Largeur du plateau
+         * @param y Hauteur du plateau
+         */
         plateauRicochet(int x, int y);
+        
+        /**
+         * @brief Récupère la grille 2D du plateau
+         * @return Référence vers la grille de jeu
+         */
         std::vector<std::vector<Case*>> getPlateau();
+        
+        /**
+         * @brief Destructeur libérant la mémoire du plateau
+         */
         ~plateauRicochet();
+        
+        /**
+         * @brief Initialise les robots sur le plateau
+         * @param robotRed Robot rouge
+         * @param robotGreen Robot vert
+         * @param robotBlue Robot bleu
+         * @param robotYellow Robot jaune
+         */
         void InitRobot(Robot* robotRed, Robot* robotGreen, Robot* robotBlue, Robot* robotYellow);
+        
+        /**
+         * @brief Crée un obstacle dans la zone spécifiée
+         * @param x1 Coordonnée X minimum
+         * @param x2 Coordonnée X maximum
+         * @param y1 Coordonnée Y minimum
+         * @param y2 Coordonnée Y maximum
+         * @param forme Type de forme pour l'objectif ("cercle", "carre", etc.)
+         */
         void setObstacle(int x1, int x2, int y1, int y2, string forme);
-        //void setObstacle(int min_x, int max_x, int min_y, int max_y);
+        
+        /**
+         * @brief Ajoute un bord en bas d'une case
+         * @param X Coordonnée X de la case
+         * @param Y Coordonnée Y de la case
+         */
         void setBordsBas(int X, int Y);
-        void setBordsHaut(int X, int Y);
-        void setBordsGauche(int X, int Y);
-        void setBordsDroit(int X, int Y);
-        void DeplacerRobot(Robot* robot, char direction);
-        void DeplacerRobotPos(Robot *robot, int Pos_X, int Pos_Y);
-        std::vector<Objectif*> getObjectifs();// Accesseur pour les objectifs
-        //void Afficher();
 
+        /**
+         * @brief Ajoute un bord en haut d'une case
+         * @param X Coordonnée X de la case
+         * @param Y Coordonnée Y de la case
+         */
+        void setBordsHaut(int X, int Y);
+
+        /**
+         * @brief Ajoute un bord à gauche d'une case
+         * @param X Coordonnée X de la case
+         * @param Y Coordonnée Y de la case
+         */
+        void setBordsGauche(int X, int Y);
+
+        /**
+         * @brief Ajoute un bord à droite d'une case
+         * @param X Coordonnée X de la case
+         * @param Y Coordonnée Y de la case
+         */
+        void setBordsDroit(int X, int Y);
+
+        /**
+         * @brief Déplace un robot dans une direction
+         * @param robot Pointeur vers le robot à déplacer
+         * @param direction Direction ('U','D','L','R')
+         */
+        void DeplacerRobot(Robot* robot, char direction);
+
+        /**
+         * @brief Déplace un robot à une position spécifique
+         * @param robot Pointeur vers le robot à déplacer
+         * @param Pos_X Nouvelle coordonnée X
+         * @param Pos_Y Nouvelle coordonnée Y
+         */
+        void DeplacerRobotPos(Robot *robot, int Pos_X, int Pos_Y);
+        
+        /**
+         * @brief Obtient la liste des objectifs
+         * @return Vecteur d'objectifs
+         */
+        std::vector<Objectif*> getObjectifs();
+
+        /**
+         * @brief Vérifie si un déplacement est valide
+         * @param x Coordonnée X de destination
+         * @param y Coordonnée Y de destination
+         * @return true si le déplacement est valide, false sinon
+         */
         bool estDeplacementValide(int x, int y);
 };
 
